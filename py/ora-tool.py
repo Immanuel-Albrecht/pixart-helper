5d1f31d87d49b3844950ab55ec0fb4be4addaa8a
Immanuel Albrecht <mail@immanuel-albrecht.de>
2020-04-15 20:40:58 +0200

    ..on my way to making this tool automatable with yamls

--- py/ora-tool.py
+++ py/ora-tool.py
@@ -67,11 +67,45 @@ if sys.argv[1] == "yaml":
             parts.append(part)
     for part in parts:
         todo.extend(find_all_ora_tool_dicts(part))
+elif sys.argv[1] == "palettize":
+    if len( sys.argv ) < 3:
+        print(f"To find out about the usage, call {sys.argv[0]} help {sys.argv[1]}.")
+        sys.exit(1)
+    inpath = sys.argv[2]
+    if len(sys.argv) > 3:
+        outpath = sys.argv[3]
+    else:
+        outpath = os.path.join(os.path.dirname(inpath),"ega-"+os.path.basename(inpath))
+    todo = [{'input': inpath,
+             'output': outpath,
+             'ops':'to-nearest-palette'}]
+elif sys.argv[1] == "binarize":
+    if len( sys.argv ) < 3:
+        print(f"To find out about the usage, call {sys.argv[0]} help {sys.argv[1]}.")
+        sys.exit(1)
+    inpath = sys.argv[2]
+    if len(sys.argv) > 3:
+        outpath = sys.argv[3]
+    else:
+        outpath = os.path.join(os.path.dirname(inpath),"a-"+os.path.basename(inpath))
+    todo = [{'input': inpath,
+             'output': outpath,
+             'ops':'to-binary-alpha'}]
+elif sys.argv[1] == "binarize":
+    if len( sys.argv ) < 3:
+        print(f"To find out about the usage, call {sys.argv[0]} help {sys.argv[1]}.")
+        sys.exit(1)
+    inpath = sys.argv[2]
+    if len(sys.argv) > 3:
+        outpath = sys.argv[3]
+    else:
+        outpath = os.path.join(os.path.dirname(inpath),"ega-a-"+os.path.basename(inpath))
+    todo = [{'input': inpath,
+             'output': outpath,
+             'ops':['to-nearest-palette','to-binary-alpha']}]
 else:
     print("Mode {sys.argv[1]} currently not available!")
     sys.exit(1)
-
-sys.exit(0)
 
 ega_palette = np.array([ [int(x[i*2]+x[i*2+1],base=16) for i in range(3)] for x in
                             map(lambda x: x.strip(), """000000
@@ -91,27 +125,24 @@ ega_palette = np.array([ [int(x[i*2]+x[i*2+1],base=16) for i in range(3)] for x 
                                                     FFFF55
                                                     FFFFFF""".split("\n"))])
 
-defaults = {}
-defaults["palette"] = ega_palette
-defaults["named-palettes"] = {'ega': ega_palette}
-defaults["to-nearest-palatte"] = {"file":"default",
-                                  "layers":lambda x: True,
-                                  "palette":"ega"}
-defaults["to-binary-alpha"] = {"file":"default",
-                                  "layers":lambda x: True,
-                                  "palette":"ega"}
+named_palettes = {'ega': ega_palette}
 
+use_all = lambda x,y=0: True
 
+default_params = {}
 
-config["palette"] = ega_palette
-config["actions"] = ["to_nearest_palette","to_binary_alpha"]
-config["input"] = sys.argv[1]
-config["output"] = sys.argv[2] if len(sys.argv) > 2 else \
-                    os.path.join(os.path.dirname(config["input"]),
-                    "ega-"+os.path.basename(config["input"]))
-config["threshold"] = 120
-config["t0"] = 0
-config["t1"] = 255
+default_params["to-nearest-palette"] = {
+    "images":use_all,
+    "layers":use_all,
+    "palette":"ega",
+    }
+    
+default_params["to-binary-alpha"] = {   
+    "images":use_all,
+    "layers":use_all,
+    "threshold":120,
+    "t0":0,
+    "t1":255}
 
 
 
@@ -120,7 +151,20 @@ def img_to_np(fp):
         reads an image from the file handle and returns it as an numpy array
     """
     img = Image.open(fp)
-    return np.asarray(img)
+    imga = np.asarray(img)
+    # the following is an odd bid to cope with non RGBA images...
+    if imga.dtype != np.uint8:
+        imga = imga
+        imga = imga / imga.max() #normalizes data in range 0 - 255
+        imga = 255 * imga
+        imga = imga.astype(np.uint8)
+    if len(imga.shape) == 2: # greyscale image
+        alpha = np.ones(imga.shape,dtype='uint8')*255
+        imga = np.stack([imga,imga,imga,alpha],axis=-1)
+    elif imga.shape[-1] == 3: # alpha missing
+        alpha = np.ones(imga.shape[:-1]+(1,),dtype='uint8')*255
+        imga = np.concatenate([imga,alpha],axis=-1)
+    return imga
 
 def load_ora(path):
     """
@@ -136,11 +180,11 @@ def load_ora(path):
         layers = [ (lbl, img_to_np(f.open(src))) for lbl,src in layer_names_srcs]
     return layers
     
-def load_single_layer(path):
+def load_single_layer(path,name="default"):
     """
         Loads a single layer png :)
     """
-    return [("layer0",img_to_np(path))]
+    return [(name,img_to_np(path))]
     
 def write_ora(path,layers):
     w = max([x[1].shape[1] for x in layers] )
@@ -219,18 +263,122 @@ def to_binary_alpha(img, threshold=120, t0=0,t1=255):
             
     return img0.reshape(shape)
 
+def transform_input_output_to_dict(x):
+    if type(x) == dict:
+        return x
+    if type(x) == list:
+        d = {}
+        for nbr,p in enumerate(x):
+            if nbr == 0:
+                d["default"] = p
+            else:
+                d[f"default{nbr}"] = p
+        return d
+    return {'default':x}
+    
 
+oplist = ["to-nearest-palette","to-binary-alpha"]
+    
+def transform_ops(x):
+    if type(x) != list:
+        x = [x]
+    ops = []
+    for op in x:
+        if type(op) == dict:
+            for k in op:
+                if not k in oplist:
+                    print(f"WARNING: ignoring unknown op {k}.")
+                    continue
+                params = default_params[k].copy()
+                if type(op[k]) == dict:
+                    for kk in op[k]:
+                        params[kk] = op[k][kk]
+                ops.append((k, params))
+        elif type(op) == list:
+            for k in op:
+                if not k in oplist:
+                    print(f"WARNING: ignoring unknown op {k}.")
+                    continue
+                params = default_params[k].copy()
+                ops.append((k, params))
+        else:
+            k = op
+            if not k in oplist:
+                print(f"WARNING: ignoring unknown op {k}.")
+                continue
+            params = default_params[k].copy()
+            ops.append((k, params))
+    return ops
 
-def work(config):
-    img = load_ora(config["input"])
-    for cmd in config["actions"]:
-        if cmd == 'to_nearest_palette':
-            img = [(name, to_nearest_palette(layer, palette=config["palette"])) 
-                        for name,layer in img]
-        elif cmd == 'to_binary_alpha':
-            img = [(name, to_binary_alpha(layer, config["threshold"],config["t0"],config["t1"])) 
-                        for name,layer in img]
-    write_ora(config["output"],img)
+def get_palette(palette):
+    if palette in named_palettes:
+        return named_palettes[palette]
+    if type(palette) == np.ndarray:
+        return palette
+    return np.array(palette, dtype="uint8")
+    
+def get_image_filter_map(filter_exp):
+    if type(filter_exp) == type(lambda:0):
+        return filter_exp
+    if type(filter_exp) == list:
+        q = [get_image_filter_map(x) for x in filter_exp]
+        return lambda x,q=q: any([f(x) for f in q])
+    if type(filter_exp) == str:
+        q = re.compile(filter_exp)
+        return lambda x,q=q: q.match(x)
 
-for c in todo:
-    work(c)
+def get_layer_filter_map(filter_exp):
+    if type(filter_exp) == type(lambda:0):
+        return filter_exp
+    if type(filter_exp) == list:
+        q = [get_layer_filter_map(x) for x in filter_exp]
+        return lambda x,y,q=q: any([f(x,y) for f in q])
+    if type(filter_exp) == str:
+        q = re.compile(filter_exp)
+        return lambda x,y,q=q: q.match(y)
+    if type(filter_exp) == int:
+        return lambda x,y,q=filter_exp: x == q
+
+def get_image_layers(data, images, layers):
+    img_layer = []
+    img_filter = get_image_filter_map(images)
+    layer_filter = get_layer_filter_map(layers)
+    for k in data:
+        if img_filter(k):
+            for nbr,l in enumerate(data[k]):
+                name, img = l
+                if layer_filter(len(data[k])-nbr-1,name):
+                    img_layer.append((k, nbr))
+    return img_layer
+
+def work(task):
+    data = {}
+    i = transform_input_output_to_dict(task["input"])
+    o = transform_input_output_to_dict(task["output"])
+    for k in i:
+        data[k] = load_ora(i[k])
+        
+    for op, params in transform_ops(task["ops"]):
+        print(f"OP: {op}")
+        for k in params:
+            print(f"  {k} = {params[k]}")
+        if op == 'to-nearest-palette':
+            p = get_palette(params["palette"])
+            for k,idx in get_image_layers(data,params["images"],params["layers"]):
+                lbl,img = data[k][idx]
+                img = to_nearest_palette(img, palette=p)
+                data[k][idx] = (lbl, img)
+        elif op == 'to-binary-alpha':
+            thr = int(params["threshold"])
+            t0 = int(params["t0"])
+            t1 = int(params["t1"])
+            for k,idx in get_image_layers(data,params["images"],params["layers"]):
+                lbl,img = data[k][idx]
+                img = to_binary_alpha(img,thr,t0,t1)
+                data[k][idx] = (lbl, img)
+            
+    for k in o:
+        write_ora(o[k],data[k])
+
+for task in todo:
+    work(task)
